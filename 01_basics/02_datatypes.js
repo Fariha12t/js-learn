b92c1590a973003354e61e0904872ea70a5ad7ca
Fariha12t <96039101+Fariha12t@users.noreply.github.com>
2023-09-28 06:26:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version
+//alert(3+3)//we aare using node js not browser
+console.log(3+3)
+console.log("fariha")
+
+let name="fariha"
+let age=20
+let loggedIn=true
+//number => 2to power 53
+//bigint
+//stringc =>""
+//boolean => true/false
+//null => standalone value ,object
+//undefined => 
+//symbol => unique
+
+//object 
+console.log(typeof "hfariha");//string
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
