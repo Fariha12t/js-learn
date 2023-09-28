5aab2c3a57267f12f51ed27c010aca410a7f957a
Fariha12t <96039101+Fariha12t@users.noreply.github.com>
2023-09-28 06:41:22 +0000

    convo of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score="33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+ //"33 "=> 33
+ //"33abc" => NaN (NOT A NUMBER)
+ //TRUE => 1; FALSE=> 0
+
+ let isLoggedIn=1
+ let booleanIsloggedIn=Boolean(isLoggedIn)
+ console.log(booleanIsloggedIn);
+ //1 => true
+ //0 => false
+ //"" => false
+ //"fariha"=> true
+
+ let someNumber=33
+ let stringNumber=String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber)
